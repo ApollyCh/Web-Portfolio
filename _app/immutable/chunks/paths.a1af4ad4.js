3aef8e963c4a8799ee4154173d2f45e1189d2708
ApollyCh <apollinaria.chernikova@gmail.com>
2023-07-10 22:49:17 +0300

    Updates

--- _app/immutable/chunks/paths.a1af4ad4.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_17rb30a)==null?void 0:s.base)??"/Web-Portfolio";var a;const t=((a=globalThis.__sveltekit_17rb30a)==null?void 0:a.assets)??e;export{t as a,e as b};
